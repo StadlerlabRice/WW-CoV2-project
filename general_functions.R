c4ed301d2fe17828dab181d78849eb66ad16214d
ppreshant <ppresh.ant@gmail.com>
2020-07-26 02:11:01 -0500

    Modified: Baylor_column to plate - Retains WWTP names, adds week name and appends to sample registry '96 well RNA' sheet - Need to modify qPCR_polished assignment to work with WWTP names instead of biobot - (split for baylor stuff, together with vol_qPCR)

--- general_functions.R
+++ general_functions.R
@@ -64,50 +64,63 @@ mutate_when <- function(data, ...)
 
 
 # Reads single column of data and converts into a 96 well plate format (Baylor sample names)
-baylor_col_to_plate <- function(flnm)
+baylor_col_to_plate <- function(sheetnm)
 {
-  bb_sheet <- c('Week 13 (7/6)')
+  target <- 'BCoV' # appends to the sample name for direct pasting in qPCR template sheet
+  flnm <- 'baylor_labels'
+  registry_sheet <- 'https://docs.google.com/spreadsheets/d/1mJcCt1wMiOuBic6sRlBZJf8KSNu2y-B5PjzCUu7jPM8/edit#gid=1011717808'
   
-  # week_match <- flnm %>% str_extract('[:digit:]+(?=-|_)') 
-  target<- 'BCoV'
-  
-  biobot_url <- 'https://docs.google.com/spreadsheets/d/1ghb_GjTS4yMFbzb65NskAlm-2Gb5M4SNYi4FHE4YVyI/edit#gid=233791008' 
-  
-  # Getting biobot names: named vector for backconversion
-  biobot_translator <- read_sheet(biobot_url, sheet = bb_sheet) %>% 
-    rename('Biobot ID' = matches('Biobot|Comments', ignore.case = T), 
-           'WWTP' = contains('SYMBOL', ignore.case = T), 
-           'FACILITY NAME' = matches('FACILITY NAME', ignore.case = T)) %>%
-    
-    drop_na(WWTP) %>% 
-    mutate('biobot_baylor' = str_replace(`Biobot ID`,'\\.', '_'), WWTP = as.character(WWTP)) %>%  
-    mutate(bb_translator = set_names(biobot_baylor , WWTP)) %>% 
-    pull(bb_translator)
+  # bb_sheet <- c('Week 13 (7/6)')
+  # 
+  # # week_match <- flnm %>% str_extract('[:digit:]+(?=-|_)') 
+  # 
+  # 
+  # biobot_url <- 'https://docs.google.com/spreadsheets/d/1ghb_GjTS4yMFbzb65NskAlm-2Gb5M4SNYi4FHE4YVyI/edit#gid=233791008' 
+  # 
+  # # Getting biobot names: named vector for backconversion
+  # biobot_translator <- read_sheet(biobot_url, sheet = bb_sheet) %>% 
+  #   rename('Biobot ID' = matches('Biobot|Comments', ignore.case = T), 
+  #          'WWTP' = contains('SYMBOL', ignore.case = T), 
+  #          'FACILITY NAME' = matches('FACILITY NAME', ignore.case = T)) %>%
+  #   
+  #   drop_na(WWTP) %>% 
+  #   mutate('biobot_baylor' = str_replace(`Biobot ID`,'\\.', '_'), WWTP = as.character(WWTP)) %>%  
+  #   mutate(bb_translator = set_names(biobot_baylor , WWTP)) %>% 
+  #   pull(bb_translator)
   
   
   # Read sample sheet
   
   baylor_names <- str_c('excel files/Baylor/', flnm, '.xlsx') %>% 
-    read_xlsx (sheet = '706-1', col_names = F) %>% 
-    select(-1) %>% 
-    setNames(c('well', 'Sample') ) %>% 
-    mutate('row' = str_match(well, '[:upper:]'), 'col' = str_match(well, '[:digit:]+')) %>% 
-    select(-well) %>% 
+    read_xlsx (sheet = sheetnm) %>% 
+    rename('Sample' = matches('Site')) %>% 
+    select('Well', 'Sample') %>% 
+    
+    mutate('row' = str_match(Well, '[:upper:]'), 'col' = str_match(Well, '[:digit:]+')) %>% 
+    select(-Well) %>% 
     
     # correct names - put a dot before replicate number
-    mutate_at('Sample', ~str_match(., '(^[:upper:]+|^[:digit:]+).*([:digit:]$)') %>% {str_c(.[,2], .[,3], sep = '.')}) %>%
+    mutate_at('Sample', ~str_match(., '(^[:upper:]+|^[:digit:]+).*([:digit:])') %>% {str_c(.[,2], .[,3], sep = '.')}) %>%
+    
     
     # substitute biobot ids in
-    mutate_at('Sample', str_replace_all , biobot_translator) %>% 
-    mutate_at('Sample', ~str_c(target, '-', .))
+    # mutate_at('Sample', str_replace_all , biobot_translator) %>% 
+    
+    # attach template name and week ID
+    mutate_at('Sample', ~str_c(target, '-', str_extract(sheetnm, '[:digit:]*(?=-[:digit:])'), '_' , .))
   
   # Convert column to table - 96 well
   
   baylor_table <- baylor_names %>% 
     pivot_wider(names_from = 'col', values_from = 'Sample') %>% 
-    rename('<>' = row)
+    rename('<>' = row) %>% 
+    mutate('-' = '', .before = 1)
   
   View(baylor_table) # manually copy paste wherever
+  
+  # write to sample registry 96 well sheet
+  sheet_append(registry_sheet, sheet = '96 well RNA', tibble(c('', '', ''))) # append 3 empty rows
+  sheet_append(registry_sheet, sheet = '96 well RNA', baylor_table)
   
 }
 
