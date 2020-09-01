ed56789623eefa586d19d771e1c226ba1b58ded9
ppreshant <ppresh.ant@gmail.com>
2020-09-01 01:43:50 -0500

    Making a vector to indicate level of shredding from David's message

--- maxwell comparisions.R
+++ maxwell comparisions.R
@@ -2,22 +2,35 @@
 
 # addendum to maxwell comparisons.Rmd
 
+# Shredding guide
+shred_guide <- c('(?<!m)82.*' = 'Qiagen_good', 
+                 'm824.*' = 'minimal.shred', 
+                 'm825 [D-F].' = 'Zirconium_minimal', 
+                 'm825 [G-I].' = 'Biofilm kit_good', 
+                 'm825 [J-M].' = 'Isopropyl_medium')
+
+
+
 # Load data ----
 
 max.data <- read_sheet(sheeturls$complete_data, sheet = '824 Maxwell') %>% 
-  mutate(RNA_extraction = 'Maxwell') 
+  mutate(RNA_extraction = 'Maxwell') %>% 
+  mutate(Beads_shredding = str_replace_all(Tube_ID, shred_guide) %>% fct_relevel('Qiagen_good', 'Biofilm kit_good', 'Isopropyl_medium', 'Zirconium_minimal', 'minimal.shred'))
+
 
 qia.data <- read_sheet(sheeturls$complete_data, sheet = '824 Rice') %>%
   filter(WWTP %in% max.data$WWTP) %>% 
   mutate(RNA_extraction = 'Qiagen')
 
-# combine data
+# combine data ----
 comb.data <- rbind(max.data, qia.data) %>% 
-  filter(!str_detect(Facility, 'Std|Vaccine|Control|NTC'))
+  filter(!str_detect(Facility, 'Std|Vaccine|Control|NTC')) %>% 
+  mutate(Beads_shredding = str_replace_all(Tube_ID, shred_guide) %>% fct_relevel('Qiagen_good', 'Biofilm kit_good', 'Isopropyl_medium', 'Zirconium_minimal', 'minimal.shred'))
+
 
 # Put maxwell and qiagen side by side
 wider_data <- comb.data %>% 
-  select(WWTP, Facility, RNA_extraction, `Target Name`, `Copies/l WW`, `Recovery fraction`) %>% 
+  select(WWTP, Facility, RNA_extraction, `Target Name`, `Copies/l WW`, `Recovery fraction`, Beads_shredding) %>% 
   group_by(WWTP, RNA_extraction, `Target Name`) %>%  mutate(index = row_number()) %>% 
   
   pivot_wider(names_from = 'RNA_extraction', values_from = c(`Copies/l WW`, `Recovery fraction`))
