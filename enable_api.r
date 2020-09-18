cdc7b3c1624d6c3ee991e1be8b7e10ce5d823ae6
David Zong <davidzong1@gmail.com>
2020-09-18 13:09:10 -0500

    get rid of old key that was shared on github

--- enable_api.r
+++ enable_api.r
@@ -1,12 +1,9 @@
+library(googlesheets4)
 # bring your own app via client id (aka key) and secret
 google_app <- httr::oauth_app(
   "wrapper",
-  key = "598444958176-ot4u688cad3ufp5mtpsn3p451ao9h5l8.apps.googleusercontent.com",
-  secret = "VlZkiFjPNRtlxe_jBujBFomd"
+  key = "keygoeshere",
+  secret = "secretgoeshere"
 )
-google_key <- "AIzaSyC2pC8Uotu765_uXEyu_diICSEEzapYVCY"
-sheets_auth_configure(app = google_app, api_key = google_key)
-
-# confirm the changes
-sheets_oauth_app()
-sheets_api_key()
+google_key <- "apikeygoeshere"
+gs4_auth_configure(app = google_app, api_key = google_key)
