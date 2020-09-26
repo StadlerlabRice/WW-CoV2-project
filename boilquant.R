c6e4e47879f14f26cf833882d05a5318c59668c7
ppreshant <ppresh.ant@gmail.com>
2020-09-26 03:48:13 -0500

    processing boilquant optimization file

---
+++ boilquant.R
@@ -0,0 +1,20 @@
+# Ad-hoc file for boilquant file processing
+
+# Name editing file
+nmchanger <- c('BCoV_M' = 'BCoV-qPCR', '^N.*' = 'BCoV-ddPCR')
+
+# Select the data of boilquants : vacboil
+boilqr <- map(long_processed_minimal, 
+             ~ filter(., str_detect(Sample_name, 'vac')) %>% 
+               mutate(Target = str_replace_all(Target, nmchanger)) %>% 
+               mutate(WWTP = as.character(WWTP) %>%  as.numeric()) %>% 
+             mutate_cond(Measurement == 'Copy #' & Target == 'BCoV-ddPCR' , across(any_of(c('value','mean')), ~ . * 100)) 
+             )
+
+{plot_mean_sd_jitter(.data_list = boilqr, 
+                     sample_var = extra_categories, exclude_sample = T, x_var = WWTP, ylabel = 'Genome copies/ul RNA') + 
+    xlab('Lysis time (min)')} %>% 
+  ggplotly(dynamicTicks = T)
+
+
+
