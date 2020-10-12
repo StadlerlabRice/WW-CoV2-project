0241e20a0cb915a3e912a6140dfb9fbf119fbb29
ppreshant <ppresh.ant@gmail.com>
2020-10-12 12:54:55 -0500

    Aligned shapes on all plots vs WWTP/ facet by method - BCoV_data_to_plot :BCoV changed to BCoV_M - work in progress on scatter plot data

--- Concentration_methods_paper.R
+++ Concentration_methods_paper.R
@@ -55,87 +55,61 @@ N_data_list <- list(raw.dat = N_data_to_plot,
 BCoV_data_to_plot <- bcov_data %>% 
   filter(!str_detect(WWTP, 'PT|NE')) %>% 
   mutate(across(Facility, ~ if_else(is.na(.x), 'Water', Facility))) %>% 
-  mutate(Target = 'BCoV') # dummy column, only necessary for plotting
+  mutate(Target = 'BCoV_M') # dummy column, only necessary for plotting
 
 bcov_data_list <- list(raw.dat = BCoV_data_to_plot,
                        summ.dat = BCoV_data_to_plot %>% 
                          group_by(across(-Fraction_recovered)) %>% 
                          summarize_all(.funs = lst(mean, sd), na.rm = T))
 
+# Join the N1N2 data with BCoV for doing scatter plot
+scatter_N_recovery <- N_data_to_plot %>% 
+  pivot_longer() # work in progress
 
 
 # Plots w jitter ----
-
-plt.N1 <- {plot_mean_sd_jitter(.data_list = N_data_list,
-                            long_format = F,
-                            target_filter = 'N1',
-                            x_var = `Concentration method`, y_var = `Copies/L WW`,
-                            colour_var = `Concentration method`,
-                            facet_var = `Concentration method`,
-                            ascending_order = T,
-                            ylabel = 'Genome copies/L Wastewater', title_text = 'SARS-CoV2 N1 across methods') + 
-    theme(legend.position = 'top', axis.text.x = element_blank())} %>% 
-  
-  format_logscale_y(.) %>% 
-  print()
-
-plt.N2 <- {plot_mean_sd_jitter(.data_list = N_data_list,
-                              long_format = F,
-                              target_filter = 'N2',
-                              x_var = `Concentration method`, y_var = `Copies/L WW`,
-                              facet_var = WWTP, colour_var = `Concentration method`,
-                              ylabel = 'Genome copies/L Wastewater', title_text = 'SARS-CoV2 N2 across methods')  + 
-    theme(legend.position = 'top', axis.text.x = element_blank())} %>% 
-  
-  format_logscale_y(.) %>% 
-  print()
-
-plt.recovery <- {plot_mean_sd_jitter(.data_list = bcov_data_list,
-                               long_format = F,
-                               target_filter = '.*',
-                               x_var = `Concentration method`, y_var = Fraction_recovered,
-                               facet_var = WWTP, colour_var = `Concentration method`,
-                               ylabel = 'Fraction of surrogate virus recovered', title_text = 'Surrogate recovery across methods')  + 
-    theme(legend.position = 'top', axis.text.x = element_blank())} %>% 
-  
-  format_logscale_y(.) %>%
-  print()
+# 
+# plt.N1 <- {plot_mean_sd_jitter(.data_list = N_data_list,
+#                             long_format = F,
+#                             target_filter = 'N1',
+#                             x_var = `Concentration method`, y_var = `Copies/L WW`,
+#                             colour_var = `Concentration method`,
+#                             facet_var = `Concentration method`,
+#                             ascending_order = T,
+#                             ylabel = 'Genome copies/L Wastewater', title_text = 'SARS-CoV2 N1 across methods') + 
+#     theme(legend.position = 'top', axis.text.x = element_blank())} %>% 
+#   
+#   format_logscale_y(.) %>% 
+#   print()
+# 
+# plt.N2 <- {plot_mean_sd_jitter(.data_list = N_data_list,
+#                               long_format = F,
+#                               target_filter = 'N2',
+#                               x_var = `Concentration method`, y_var = `Copies/L WW`,
+#                               facet_var = WWTP, colour_var = `Concentration method`,
+#                               ylabel = 'Genome copies/L Wastewater', title_text = 'SARS-CoV2 N2 across methods')  + 
+#     theme(legend.position = 'top', axis.text.x = element_blank())} %>% 
+#   
+#   format_logscale_y(.) %>% 
+#   print()
+# 
+# plt.recovery <- {plot_mean_sd_jitter(.data_list = bcov_data_list,
+#                                long_format = F,
+#                                target_filter = '.*',
+#                                x_var = `Concentration method`, y_var = Fraction_recovered,
+#                                facet_var = WWTP, colour_var = `Concentration method`,
+#                                ylabel = 'Fraction of surrogate virus recovered', title_text = 'Surrogate recovery across methods')  + 
+#     theme(legend.position = 'top', axis.text.x = element_blank())} %>% 
+#   
+#   format_logscale_y(.) %>%
+#   print()
 
 # Plots w shapes ----
-
-
-shape_plt_N1 <- {N_data_to_plot %>% 
-  filter(str_detect(Target, 'N1')) %>% 
-  ggplot(aes(`Concentration method`, `Copies/L WW`, colour = `Concentration method`,  shape = WWTP)) + 
-  geom_jitter() + 
-  ylab('Genome copies/L wastewater') + ggtitle('SARS-CoV2 N1 across methods') + 
-    scale_shape_manual(values = c(15,16,17,7,8,10,3))} %>% 
-  format_logscale_y() %>%
-  format_classic() %>% 
-  print()
-
-shape_plt_N2 <- {N_data_to_plot %>% 
-    filter(str_detect(Target, 'N2')) %>% 
-    ggplot(aes(`Concentration method`, `Copies/L WW`, colour = `Concentration method`,  shape = WWTP)) + 
-    geom_jitter() + 
-    ylab('Genome copies/L wastewater') + ggtitle('SARS-CoV2 N2 across methods') + 
-    scale_shape_manual(values = c(15,16,17,7,8,10,3))} %>% 
-  format_logscale_y() %>%
-  format_classic() %>% 
-  print()
-
-
-shape_plt_recovery <- {BCoV_data_to_plot %>% 
-  ggplot(aes(`Concentration method`, Fraction_recovered, colour = `Concentration method`, shape = WWTP)) + 
-  geom_jitter() + 
-  ylab('Fraction of surrogate virus recovered') + ggtitle('Surrogate recovery across methods')} %>% 
-  format_classic(.) %>% 
-  print()
 
 aligned_shape_plt_N1 <- {N_data_to_plot %>% 
     filter(str_detect(Target, 'N1')) %>% 
     ggplot(aes(WWTP, `Copies/L WW`, colour = `Concentration method`,  shape = WWTP)) + 
-    geom_jitter(width = .2) + 
+    geom_point() + 
     facet_grid(~`Concentration method`) +
     ylab('Genome copies/L wastewater') + ggtitle('SARS-CoV2 N1 across methods') + 
     scale_shape_manual(values = c(15,16,17,7,8,10,3))} %>% 
@@ -143,17 +117,34 @@ aligned_shape_plt_N1 <- {N_data_to_plot %>%
   format_classic() %>% 
   print()
 
+aligned_shape_plt_N2 <- {N_data_to_plot %>% 
+    filter(str_detect(Target, 'N2')) %>% 
+    ggplot(aes(WWTP, `Copies/L WW`, colour = `Concentration method`,  shape = WWTP)) + 
+    geom_point() +
+    facet_grid(~`Concentration method`) +
+    ylab('Genome copies/L wastewater') + ggtitle('SARS-CoV2 N2 across methods') + 
+    scale_shape_manual(values = c(15,16,17,7,8,10,3))} %>% 
+  format_logscale_y() %>%
+  format_classic() %>% 
+  print()
 
-# Data output ----
+aligned_shape_plt_recovery <- {BCoV_data_to_plot %>% 
+    ggplot(aes(WWTP, Fraction_recovered, colour = `Concentration method`, shape = WWTP)) + 
+    geom_point() + 
+    facet_grid(~`Concentration method`) +
+    ylab('Fraction of surrogate virus recovered') + ggtitle('Surrogate recovery across methods')} %>% 
+  format_classic(.) %>% 
+  print()
+
+# Plot save ----
 # (optional)
 
-# plots-1
+# plots-w-jitter
 # ggsave('qPCR analysis/Methods paper/Methods_paper_N1.pdf', plot = plt.N1, width = 8, height = 4)
 # ggsave('qPCR analysis/Methods paper/Methods_paper_N2.pdf', plot = plt.N2, width = 8, height = 4)
 # ggsave('qPCR analysis/Methods paper/Methods_paper_Recovery.pdf', plot = plt.recovery, width = 8, height = 4)
 
-# plots-kiara
-ggsave('qPCR analysis/Methods paper/Methods_paper_N1.pdf', plot = shape_plt_N1, width = 8, height = 4)
-ggsave('qPCR analysis/Methods paper/Methods_paper_N2.pdf', plot = shape_plt_N2, width = 8, height = 4)
-ggsave('qPCR analysis/Methods paper/Methods_paper_Recovery.pdf', plot = shape_plt_recovery, width = 8, height = 4)
-ggsave('qPCR analysis/Methods paper/Methods_paper_Recovery.N1-aligned.pdf', plot = aligned_shape_plt_N1, width = 8, height = 4)
+# plots-w-shapes
+ggsave('qPCR analysis/Methods paper/WWTP by shape/Methods_paper_Recovery.N1-aligned.pdf', plot = aligned_shape_plt_N1, width = 8, height = 4)
+ggsave('qPCR analysis/Methods paper/WWTP by shape/Methods_paper_Recovery.N2-aligned.pdf', plot = aligned_shape_plt_N2, width = 8, height = 4)
+ggsave('qPCR analysis/Methods paper/WWTP by shape/Methods_paper_Recovery.Recovery-aligned.pdf', plot = aligned_shape_plt_recovery, width = 8, height = 4)
