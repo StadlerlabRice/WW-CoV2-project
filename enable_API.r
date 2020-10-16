769a74923c971dae62abc1f65a0aeaf1137fa642
ppreshant <ppresh.ant@gmail.com>
2020-10-16 08:05:32 -0500

    enable_API: Updating for easy entering of key locally (in console)

--- enable_API.r
+++ enable_API.r
@@ -1,12 +1,15 @@
 # Author: David Zong. Date: 18/Sep/20
 # Purpose - to get googlesheets4 to use a google cloud account to increase quote limits
 
+if(!exists('key_this')) key_this <- "keygoeshere"
+if(!exists('secret_this')) secret_this <- "secretgoeshere"
+
 library(googlesheets4)
 # bring your own app via client id (aka key) and secret
 google_app <- httr::oauth_app(
   "wrapper",
-  key = "keygoeshere",
-  secret = "secretgoeshere"
+  key = key_this,
+  secret = secret_this
 )
 google_key <- "apikeygoeshere"
 gs4_auth_configure(app = google_app, api_key = google_key)
