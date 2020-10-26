be82e281c61a41289ef7577d7fcdc58c60625bc6
ppreshant <ppresh.ant@gmail.com>
2020-10-26 03:16:32 -0500

    Pavan's adhoc data filtering and putting into multiple sheets

---
+++ pavan_BR_filtering_multiple_sheets.R
@@ -0,0 +1,19 @@
+
+d.pavan <- presentable_data %>% filter(str_detect(Facility, '102./.R.*') | str_detect(Tube_ID, '1020 (B|D|E|F)'))
+  
+
+d.interim <- d.pavan %>% 
+  mutate(catcol = str_extract(WWTP, '.R'), .after = WWTP) %>% 
+  
+  mutate_cond(str_detect(Tube_ID, '1020 (B|D|E|F).$'), catcol  = str_replace(Tube_ID, '1020 (.)[:digit:]', '\\1R')) %>% 
+
+  group_by(catcol) %>% 
+  arrange(catcol, `Target Name`, Tube_ID, WWTP) %>% 
+  nest()
+
+map2(d.interim$data, d.interim$catcol, 
+     ~ write_sheet(.x, sheet = .y, 
+                   ss = 'https://docs.google.com/spreadsheets/d/1g6-Z0kpItuffidKEzjv-jvcTHzZPUQp9LxyTeKLngV4/edit#gid=0')
+)
+
+  
