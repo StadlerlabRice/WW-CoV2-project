17a8da13894039cd7f268f50f471a534e3d8b928
ppreshant <ppresh.ant@gmail.com>
2020-10-31 01:32:59 -0500

    Preventing writing of N1,N2 runs of Vaccine to the data dump (tested)

--- processing_functions.R
+++ processing_functions.R
@@ -332,7 +332,7 @@ process_ddpcr <- function(flnm = flnm.here, baylor_wells = 'none', adhoc_dilutio
   
   
   # Saving vaccine data into Vaccines sheet in data dump: For easy book keeping
-  vaccine_data <- polished_results %>% filter(str_detect(Sample_name, 'Vaccine')) %>%
+  vaccine_data <- polished_results %>% filter(str_detect(Sample_name, 'Vaccine') & !str_detect(Target, 'N._multiplex')) %>%
     mutate('Prepared on' = '',
            Week = str_extract(flnm, '[:digit:]{3,4}') %>% unlist() %>% str_c(collapse = ', '),
            Vaccine_ID = assay_variable, 
