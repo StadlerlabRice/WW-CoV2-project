e83183446d7be73cbbc72201317a289c641c1f3f
ppreshant <ppresh.ant@gmail.com>
2020-11-03 01:27:51 -0600

    processing_functions: vectorized template volume assignment of ddpcr. Corrected mistake in BCoV correction values since dd.WW64 to dd.WW70 - Works

--- processing_functions.R
+++ processing_functions.R
@@ -256,13 +256,10 @@ process_qpcr <- function(flnm = flnm.here, std_override = NULL, baylor_wells = '
 process_ddpcr <- function(flnm = flnm.here, baylor_wells = 'none', adhoc_dilution_wells = 'none')
 { # Baylor wells : choose 1) none, 2) '.*' for all, 3) '[A-H]([1-9]$|10)' etc. for specific wells 
   
-  template_volume_dpcr <- 10 /22 * 20 # ul template volume per well of the 20 ul ddPCR reaction - for N1/N2
-  # template_volume_ddpcr <- tibble(Target = c('N1_multiplex', 'N2_multiplex', 'BCoV2'),
-  #                                 template_vol = c(10, 10, 4) /22 * 20) # ul template volume per well of the 20 ul ddPCR reaction for each target
+  template_volume_ddpcr <- tibble(Target = c('N1_multiplex', 'N2_multiplex', 'BCoV', 'pMMoV'),
+                                  template_vol = c(10, 10, 4, 4) /22 * 20) # ul template volume per well of the 20 ul ddPCR reaction for each target
   
-  template_volume_correction_BCoV <- 4 / 22 * 20 / template_volume_dpcr # BCoV reaction has only 4 ul instead of 10 ul in a 22 ul reaction
   RNA_dilution_factor_BCoV <- 50  # RNA dilution factor for diluted BCoV samples
-  total_correction_factor_BCoV <- RNA_dilution_factor_BCoV * template_volume_correction_BCoV # total correction factor to implement for BCoV samples 
   
   # Ad hoc - marking the samples from baylor (will append /baylor to target name)
   # Work in progress?
@@ -294,7 +291,11 @@ process_ddpcr <- function(flnm = flnm.here, baylor_wells = 'none', adhoc_dilutio
     right_join(plate_template, by = 'Well Position') %>%  # Incorporate samples names from the google sheet by matching well position
     mutate_at('Target', ~str_replace_all(., c('N1' = 'N1_multiplex' , 'N2' = 'N2_multiplex'))) %>% 
     filter(!is.na(Target)) %>% 
-    mutate('Copy #' = CopiesPer20uLWell/ template_volume_dpcr) %>% 
+    
+    # Adding different template volumes for each target for division
+    left_join(template_volume_ddpcr) %>% # join array of template volume - different for N1,N2 and BCOV2
+    mutate('Copy #' = CopiesPer20uLWell/ template_vol) %>%  
+    
     select(`Sample_name`, `Copy #`, Target, everything())
   
   
@@ -309,7 +310,7 @@ process_ddpcr <- function(flnm = flnm.here, baylor_wells = 'none', adhoc_dilutio
     mutate_at('assay_variable', as.character) %>% 
     mutate_at('biological_replicates', ~str_replace_na(., '')) %>% 
     
-    mutate(across(`Copy #`, ~ if_else(str_detect(Target, 'BCoV'), .x * total_correction_factor_BCoV, .x))) %>% # Correcting for template dilution in case of BCoV ddPCRs
+    mutate(across(`Copy #`, ~ if_else(str_detect(Target, 'BCoV'), .x * RNA_dilution_factor_BCoV, .x))) %>% # Correcting for template dilution in case of BCoV ddPCRs
     
     
     # Ad-hoc corrections for errors in making plate - sample dilutions etc.
