39a210f845823a59bde7ccdfb85cdc18b0671aaa
ppreshant <ppresh.ant@gmail.com>
2020-11-30 06:01:46 -0600

    ntc_droplets: making a list first, selecting only useful columns before merging into data frame, adding summary statistics

--- ntc_droplets.r
+++ ntc_droplets.r
@@ -10,12 +10,15 @@ sheets_in_dump <- sheet_names(ss = sheeturls$data_dump)
 dd_sheet_names <- sheets_in_dump %>% .[str_detect(., 'dd.')] 
 
 # get all ddPCR sheets
-all_dd.data <- map_dfr(dd_sheet_names, ~ read_sheet(sheeturls$data_dump, sheet = .x) %>% 
+all_dd.data_list <- map(dd_sheet_names, ~ read_sheet(sheeturls$data_dump, sheet = .x) %>% 
                          rename(Sample_name = any_of('Sample Name')) %>% # if name is according to old convension, this will rename it 
-                         select(-matches('Conc')) %>% 
-                         mutate(across('Sample_name', as.character)) %>% 
+                         select(-matches('Conc'), -biological_replicates) %>% 
+                         mutate(across(where(is.list), as.character)) %>% 
                          mutate('Run_ID' = str_extract(.x, '(?<=dd\\.WW)[:digit:]+'), .before = 1)
 )
+
+all_dd.data <- map(all_dd.data_list, ~ select(.x, Run_ID, Sample_name, Target, Positives, any_of('AcceptedDroplets'), `Copy #`)) %>% 
+  bind_rows()
 
 
 # Data filtering ----
@@ -28,10 +31,11 @@ ntc_dd.data <- all_dd.data %>%
 # Plotting ----
 
 # calling r markdown file
-rmarkdown::render('ntc_droplets.Rmd', output_file = str_c('./qPCR analysis/', 'NTC droplets', '.html'))
+rmarkdown::render('ntc_droplets.Rmd', output_file = str_c('./qPCR analysis/Extra graphs/', 'NTC droplets', '.html'))
 
+# 
+# # testing 
+# tst <- read_sheet(sheeturls$data_dump, sheet = 'dd.WW73_1109_N1N2')
+# map_chr(tst, class) %>% view()
+# view(tst)
 
-# testing 
-tst <- read_sheet(sheeturls$data_dump, sheet = 'dd.WW73_1109_N1N2')
-map_chr(tst, class) %>% view()
-view(tst)
