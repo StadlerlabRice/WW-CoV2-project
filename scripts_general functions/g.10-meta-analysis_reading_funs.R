a258e550268aebe7f4324acfefad0958f7d860f5
ppreshant <ppresh.ant@gmail.com>
2021-03-29 18:20:04 -0500

    g.10-meta-analysis_reading_funs : Skeleton for function to read columns from data dump or complete data for meta-analysis. All column name changes will be incorporated here as rename calls

---
+++ scripts_general functions/g.10-meta-analysis_reading_funs.R
@@ -0,0 +1,12 @@
+# Metaanalysis reading functions
+# Specific reading functions for meta-analysis across runs, across weeks etc.
+# example : Looking at NTCs across weeks / Looking for trends in specific or all sites across weeks
+
+
+# read from data dump ----
+# -> This is going to be obsolete soon, when data dump is not going to be written anymore
+
+
+
+# Read from complete data ----
+
