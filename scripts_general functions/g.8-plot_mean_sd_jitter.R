63a5211dfd3a9038d537f5fdf32afb30cac58395
ppreshant <ppresh.ant@gmail.com>
2021-11-29 23:31:02 -0600

    g.8-plot_mean_sd_jitter : documenting arguments, folding lines - tested that it works with csv loaded data

--- scripts_general functions/g.8-plot_mean_sd_jitter.R
+++ scripts_general functions/g.8-plot_mean_sd_jitter.R
@@ -1,31 +1,55 @@
 
-# Plotting mean, sd and individual replicates in a jitter form
+# Plotting mean, sd and individual replicates in a jitter form (adds random jitter so that points don't overlap)
 # Yet to be fully generalized function. But should work for any data frame by adding dummy columns. 
 # Default parameters correspond to COVID-qPCR files
 
-plot_mean_sd_jitter <- function(.data = presentable_data, 
-                                long_format = FALSE,
-                                
-                                measure_var = 'Copy #', 
-                                colour_var = Target_Name, x_var = assay_variable, y_var = Copies_Per_Liter_WW, 
-                                facet_var = NULL, 
-                                
-                                unique_columns_to_incl = c('Facility'), # columns to determine uniqueness for calc mean ...
-                                # ... will automatically include the x_var and colour_var
-                                
-                                # Filtering variables
-                                sample_filtering_var = '.*', exclude_sample = F, 
-                                WWTP_filtering_var = wwtp_manhole_names, exclude_WWTP = F, 
-                                target_filter_var = '.*', 
-                                
-                                ascending_order = FALSE, 
-                                title_text = title_name, ylabel = 'Genome copies/ul RNA', xlabel = plot_assay_variable, 
-                                
-                                # additional aesthetics
-                                facet_style = 'grid',
-                                show_LOD = FALSE,
-                                
-                                print_the_plot = TRUE)
+plot_mean_sd_jitter <- 
+  function(.data = presentable_data, # this is the data.frame/ tibble to be plotted
+           
+           
+           # Key variables to plot
+           x_var = assay_variable, # make this variable as the x-axis
+           y_var = Copies_Per_Liter_WW,  # make this variable as the y-axis
+           colour_var = Target_Name,  # Colour points/lines by this variable
+           facet_var = NULL,  # Splite into small-multiple of plots for each value of this variable
+           
+           
+           # Grouping variables to determine mean within the each group
+           unique_columns_to_incl = c('Facility'), # columns to determine uniqueness for calculating mean ...
+           # ... will automatically include the x_var and colour_var
+           
+           
+           # Filtering variables : filter data to only plot a subset 
+           # uses regular expressions (REGEX) that are case sensitive for filtering
+           # exclude_.. is useful to toggle switch :Should these samlpes should be ones plotted or the ones excluded
+           sample_filtering_var = '.*', exclude_sample = F,  # filtering the Sample_name column to match this string
+           WWTP_filtering_var = wwtp_manhole_names, exclude_WWTP = F, # filtering the WWTP column
+           target_filter_var = '.*', # filtering the Target_Name column. 
+           # Ex: N1|N2 ; pMMoV ; BCoV (get the capitalization correct)
+           
+           # Ordering the x-axis of the plots
+           ascending_order = FALSE, 
+           
+           
+           # Plot labels
+           title_text = title_name, 
+           ylabel = 'Genome copies/ul RNA', 
+           xlabel = plot_assay_variable, 
+           
+           
+           # additional aesthetics
+           facet_style = 'grid', # There is either grid or wrap (experimental feature, not used)
+           show_LOD = FALSE, # experimental feature, to show LOD -- doesn't work if LOD is not constant 
+           
+           # Do you want to print the plot, use FALSE if you will add stuff to the plot before showing it
+           print_the_plot = TRUE,
+           
+           
+           # obsolete parameters, ignore.
+           # used when raw_data and mean_sd data were merged as a list and sent into this function                               
+           long_format = FALSE,  
+           measure_var = 'Copy #' # The variable from the long_format to plot
+  ) 
 
 { # Convenient handle for repetitive plotting of individual replicates as points, and mean as horizontal bars
   # Specify data format: typically wide vs long which is a special case (specify in long_format = FALSE or TRUE)
@@ -34,11 +58,17 @@ plot_mean_sd_jitter <- function(.data = presentable_data,
   # filtering data to be plotted by user inputs
   .dat_filtered <- 
     .data %>% 
-    filter(., if('Sample_name' %in% colnames(.)) str_detect(`Sample_name`, sample_filtering_var, negate = exclude_sample) else TRUE, 
-           if('WWTP' %in% colnames(.)) str_detect(WWTP, WWTP_filtering_var, negate = exclude_WWTP) else TRUE, 
+    filter(., 
+           if('Sample_name' %in% colnames(.)) { # filtering only if the column exists
+             str_detect(`Sample_name`, sample_filtering_var, negate = exclude_sample) }else TRUE, 
+           
+           if('WWTP' %in% colnames(.)) {
+             str_detect(WWTP, WWTP_filtering_var, negate = exclude_WWTP)} else TRUE, 
+           
            str_detect(Target_Name, target_filter_var))
   
-  mean_y_var_str <- expr(!!str_c('mean_', deparse(enexpr(y_var)) ) ) # make a string to use mean_{{y_var}} in regular functions
+  # make a string to use mean_{{y_var}} in regular functions
+  mean_y_var_str <- expr(!!str_c('mean_', deparse(enexpr(y_var)) ) ) 
   
   
   # Long format : filtering data to be plotted by user inputs : ---- OBSOLETE ; do not use
@@ -87,7 +117,8 @@ plot_mean_sd_jitter <- function(.data = presentable_data,
     geom_jitter(mapping = aes(alpha = map_chr({{y_var}}, ~. == 0), 
                               size = map_chr({{y_var}}, ~. == 0)), 
                 width = .2, show.legend = F ) +
-    scale_alpha_manual(values = c(.3, 1)) + scale_size_manual(values = c(1, 2)) + # manual scale for emphasizing unamplified samples
+    # manual scale for emphasizing unamplified samples
+    scale_alpha_manual(values = c(.3, 1)) + scale_size_manual(values = c(1, 2)) + 
     
     # # Plotting actual spike ins (only for Recovery plot'; only with long format data )
     # { if(measure_var == 'Recovered') list(geom_point(data = summ_actual_spike_in, colour = 'black', shape = 21), 
@@ -97,7 +128,8 @@ plot_mean_sd_jitter <- function(.data = presentable_data,
     # Facetting
     facet_grid(cols = vars({{facet_var}}), scales = 'free_x', space = 'free_x') +
     
-    # experimental - conditional facetting (doesn't work for unknown reasons) : Just facet_grid() the output to remove facets or add new!
+    # experimental - conditional facetting (doesn't work for unknown reasons) : 
+    # Just facet_grid() the output to remove facets or add new!
     # { if (facet_style == 'grid') list(facet_grid(cols = vars({{facet_var}}), scales = 'free_x', space = 'free_x'))
     #   if (facet_style == 'wrap free') list(facet_wrap(facets =  vars({{facet_var}}), scales = 'free')) 
     #   else NULL
