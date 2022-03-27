67b2ec0793a0b59e28f665987d56ab9bf32b0359
ppreshant <ppresh.ant@gmail.com>
2022-03-26 22:19:16 -0500

    g.5-mathematical_fitting_funs : added comments, explained 'trig' input var

--- scripts_general functions/g.5-mathematical_fitting_funs.R
+++ scripts_general functions/g.5-mathematical_fitting_funs.R
@@ -4,7 +4,6 @@
 
 
 # getting regression line and R2 values to put into the standard curve plot 
-# Source: https://stackoverflow.com/a/7549819/9049673
 
 # least square fitting - linear regression (typical use: for qPCR standard curve)
 lm_std_curve <- function(df, trig = 0)
@@ -16,14 +15,21 @@ lm_std_curve <- function(df, trig = 0)
 
 
 # Extract linear regression equation and parameters from the fit by lm
+# Source: https://stackoverflow.com/a/7549819/9049673
+
 lm_eqn <- function(m, trig = 0){
+  # use trig = 'coeff' to output data frame of the fitting coefficients 
+  # trig = 0 for output of equation (for pasting on the plot)
   
+  # format the equation "y = mx + c, R^2 = Rsquare value" as text to put on plot
   eq <- substitute(italic(y) == b %.% italic(x)+ a*","~~italic(r)^2~":"~r2, 
                    list(a = format(unname(coef(m)[1]), digits = 4), 
                         b = format(unname(coef(m)[2]), digits = 3), 
                         r2 = format(summary(m)$r.squared, digits = 3)))
-  # if(trig == 'coeff') c(round(coef(m)[2], 2), round(summary(m)$r.squared, 2))
+  
+  # output a table with slope, y_intercept and r_square rounded off
   if(trig == 'coeff') tibble(slope = round(coef(m)[2], 2), y_intercept = round(coef(m)[1], 2), r_square = round(summary(m)$r.squared, 3))
+  
   else as.character(as.expression(eq)); 
 }
 
