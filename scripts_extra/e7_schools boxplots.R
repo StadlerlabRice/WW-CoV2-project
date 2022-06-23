5fb06b16fe28521778318431ac13a210c8d575d0
ppreshant <ppresh.ant@gmail.com>
2022-06-23 10:40:56 -0500

    e7_schools boxplots : Modifications avg replicates - using column Grade_Level_2 in school metadata - averaging N1, N2 and two replicates for concise plotting - ordering grade levels properly - adding text annotations when missing - Saving all ggplots

--- scripts_extra/e7_schools boxplots.R
+++ scripts_extra/e7_schools boxplots.R
@@ -18,7 +18,9 @@ all_data <- read_xlsx('excel files/boxplots_for_LS_7-4-22/Wastewater Surveillanc
 #              by = 'school_id')
 
 # loading the custom generated file with school names and symbols
-school_metadata <- read_csv('excel files/boxplots_for_LS_7-4-22/schools_metadata.csv')
+school_metadata <- read_csv('excel files/boxplots_for_LS_7-4-22/schools_metadata.csv') %>% 
+  # reducing redundant columns -- Sharpstown International only one different : Elementary/Secondary and High school
+  select(-Grade_Level) %>% rename(Grade_Level = Grade_Level_2)  # retain Grade_Level_2 and rename to Grade_Level
 
 # View data
 # view(school_data)
@@ -43,10 +45,24 @@ school_rice.data <-
   arrange(Enrollment_as_Oct2019) %>% 
   mutate(across(c('Facility', 'Symbol'), # freeze the order of samples 
                 ~ fct_inorder(.x))) %>% 
-
+ 
+  # Collect all replicates and N1-N2 under the same column
   pivot_longer(cols = starts_with('Rep'),
                names_to = 'Replicate',
-               values_to = 'Copies_per_litre')
+               values_to = 'Copies_per_litre') %>% 
+  
+  # order the grade levels for proper plotting
+  mutate(across(Grade_Level, ~ fct_relevel(.x, 'Elementary', 'Middle', 'High School')))
+
+# Average replicates and N1, N2
+school_averaged <- 
+  group_by(school_rice.data, across(-c(Copies_per_litre, Replicate))) %>% 
+  summarize(across(Copies_per_litre, mean, na.rm = TRUE)) # takes the mean of stuff
+  
+  # group_by(Facility, Date) %>% 
+  # mutate(across(Copies_per_litre, mean, na.rm = TRUE)) %>% 
+  # select(-Replicate)
+  
 
 # function to label the number of data points on the plot
 # Copied from stackoverflow : https://stackoverflow.com/a/31140454/9049673
@@ -58,6 +74,9 @@ give.n <- function(x){
 # Plots ----
 
 # make box and dot plot
+
+# boxplot all ----
+
 plt.general <- 
   {ggplot(school_rice.data,
           aes(x = Replicate, y = Copies_per_litre,
@@ -73,11 +92,11 @@ plt.general <-
                  position = position_jitterdodge(dodge.width = .75, 
                                                  jitter.width = .12, jitter.height = 0)) + 
       # text labels for number of points
-      # stat_summary(fun.data = give.n, geom = 'text',
-      #              position = position_dodge2(width = .75)) +
+      stat_summary(fun.data = give.n, geom = 'text',
+                   position = position_dodge2(width = .75)) +
       
       # annotations
-      annotation_logticks() +  # tick marks to indicate log y axis
+      annotation_logticks(sides = 'l') +  # tick marks to indicate log y axis
       
       ggtitle('School data by grade : till 7th April, 2022',
               subtitle = 'Number of data points shown above the median line. Whiskers show 25% and 75% quartiles') +
@@ -90,6 +109,43 @@ plt.general <-
   
   print()
 
+
+# Boxplot avg ----
+
+# boxplot of average of replicates and targets
+plt.avg <- 
+  {ggplot(school_averaged,
+          aes(x = 1, y = Copies_per_litre,
+              fill = Grade_Level,
+              label = Facility)) + 
+      
+      # boxplot
+      geom_boxplot(outlier.shape = NA, alpha = .6) +
+      
+      # points, black outline, semi transparent, position matches the boxplot, scattered along x-axis for visual 
+      geom_point(shape = 21,
+                 alpha = 0.2, 
+                 position = position_jitterdodge(dodge.width = .75, 
+                                                 jitter.width = .12, jitter.height = 0)) + 
+      # text labels for number of points
+      stat_summary(fun.data = give.n, geom = 'text',
+                   position = position_dodge2(width = .75)) +
+
+      # annotations
+      annotation_logticks(sides = 'l') +  # tick marks to indicate log y axis
+      
+      ggtitle('School data by grade : till 7th April, 2022',
+              subtitle = 'Number of data points shown above the median line. Whiskers show 25% and 75% quartiles') +
+      
+      theme(legend.position = 'top')
+    
+  } %>% 
+  
+  format_logscale_y() %>%
+  
+  print()
+
+# by enrollment ----
 
 # Each school - by enrollment 
 plt.eachschoolenrollment <- 
@@ -126,11 +182,12 @@ plt.eachschoolenrollment <-
   print()
 
 
+# by each school ----
+
 # plot by each school - ordered by enrollment
 plt.eachschool <- 
-  {ggplot(school_rice.data,
+  {ggplot(school_averaged,
           aes(x = Copies_per_litre, y = Symbol,
-              colour = Replicate,
               label = Facility)) + 
       
       # points, black outline, semi transparent, position matches the boxplot, scattered along x-axis for visual 
@@ -144,7 +201,8 @@ plt.eachschool <-
                 show.legend = F) + 
       
       ggtitle('School data by enrollment : till 7th April, 2022',
-              subtitle = 'School enrollment as of Oct2019 shown on the left') +
+              subtitle = 'School enrollment numbers as of Oct2019 are shown on the left') +
+      ylab('School name (abbreviated)') +
       
       theme(legend.position = 'top')
     
@@ -157,8 +215,12 @@ plt.eachschool <-
 
 # Save plots ----
 
-ggsave(str_c('qPCR analysis/', title_name, '.png'),
-       width = 6, height = 8)
+# ggsave(str_c('qPCR analysis/', title_name, '.png'), width = 6, height = 8)
+
+ggsave(plot_as('Schools by grade_7-April-22'), plt.general, width = 5, height = 6)
+ggsave(plot_as('Schools by grade-Avg_7-April-22'), plt.avg, width = 4, height = 6)
+ggsave(plot_as('Schools_by school ordered by enrollment_7-April-22'), plt.eachschool, width = 6, height = 9)
+
 
 
 # save plots into html file
