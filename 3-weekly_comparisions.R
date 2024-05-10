021b7ab43f91a718b5e03cfef78cbff5a257ac9c
kmp11 <kmp11@rice.edu>
2024-05-10 13:30:41 -0500

    Added RESP and PATH to QX-600 keywords. Also added more colors for pmmov charting.

--- 3-weekly_comparisions.R
+++ 3-weekly_comparisions.R
@@ -13,7 +13,8 @@ read_these_sheets <- c('091123 Rice Schools Sampler Test pMMoV', '091823 Rice Sc
                        '102323 - 102523 Rice Schools and Congregate pMMoV', '103023 - 110123 Rice Schools and Congregate pMMoV',
                        '110623 - 111523 Rice Schools and Congregate pMMoV', '112123 - 112923 Rice Schools and Congregate pMMoV',
                        '120423 - 121323 Rice Schools and Congregate pMMoV', '121223 - 010324 Rice Schools and Congregate pMMoV',
-                       '010824 - 012424 Rice Schools and Congregate PMMoV')
+                       '010824 - 012424 Rice Schools and Congregate PMMoV', '021324 - 022824 Rice Schools and Congregate PMMoV',
+                       '031824 - 032024 Rice Schools and Congregate PMMoV', '040124 - 041724 Rice Schools and Congregate PMMoV')
 
 # if you are interested in only pMMoV stuff, then make this TRUE
 filter_for_pMMoV_data <- TRUE # optional filtering for samples that have pMMoV
